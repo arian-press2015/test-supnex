2add986dd6e3adbafb42aa2e7abff07eba4c7248
AP2015 <arian.press2015@gmail.com>
2023-06-04 23:22:19 +0330

    feat: add docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ProductModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/testproject'),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 'mongodb://localhost:27017/testProject',
+    ),
   ],
   controllers: [],
   providers: [],
